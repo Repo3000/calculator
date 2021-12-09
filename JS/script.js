087a9670183f3a4557288552f684f90812e5bc54
gr206 <rodriguez.gabriel@protonmail.com>
2021-12-09 13:35:42 -0800

    first init. All arithmatic functions work

---
+++ JS/script.js
@@ -0,0 +1,59 @@
+//this is addition
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+const num1 = parseInt(prompt("Enter a number:"));
+
+const num2 = parseInt(prompt("Enter another number:"));
+
+
+//this alert will pop up// 
+alert(add(num1, num2));
+
+
+//subtraction below
+function subtract(sub1, sub2) {
+  return sub1 - sub2;
+}
+const sub1 = parseInt(prompt("Enter a number:"));
+
+const sub2 = parseInt(prompt("Enter another number:"));
+
+
+//this alert will pop up// 
+alert(subtract(sub1, sub2));
+
+//multiply
+function multiply(mul1, mul2) {
+  return mul1 * mul2;
+}
+const mul1 = parseInt(prompt("Enter a number:"));
+
+const mul2 = parseInt(prompt("Enter another number:"));
+
+
+//this alert will pop up// 
+alert(multiply(mul1, mul2));
+
+
+//Divide
+
+function divide(div1, div2) {
+  return div1 / div2;
+}
+const div1 = parseInt(prompt("Enter a number:"));
+
+const div2 = parseInt(prompt("Enter another number:"));
+
+
+//this alert will pop up// 
+alert(divide(div1, div2));
+
+
+
+
+
+
+
+
